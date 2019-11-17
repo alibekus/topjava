2a393af72b60910689ac9ef8ffa31a4fcb8c0ace
Alibek Akbarov <alibek@akbar.kz>
2019-11-17 21:45:54 +0600

    Lesson 6. Patch 8. Add test validation. Corrected grammar.
    Corrected grammar in the ValidationUtil and AbstractServiceTest.

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -47,7 +47,6 @@ public class ValidationUtil {
     public static Throwable getRootCause(Throwable t) {
         Throwable result = t;
         Throwable cause;
-
         while (null != (cause = result.getCause()) && (result != cause)) {
             result = cause;
         }
