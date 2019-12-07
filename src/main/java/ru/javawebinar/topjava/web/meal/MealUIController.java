96ebaf0c2ea5818120972561a520ab656c219753
Alibek Akbarov <alibek@akbar.kz>
2019-12-07 16:08:03 +0600

    Lesson 8. HW8. Realization 3. Spring Security fix.
    Fixed spring-security.xsd retrieving for last Spring Security version.

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -16,38 +16,38 @@ import java.util.List;
 @RequestMapping("/ajax/profile/meals")
 public class MealUIController extends AbstractMealController {
 
-@Override
-@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-public List<MealTo> getAll() {
-    return super.getAll();
-}
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
 
-@Override
-@DeleteMapping(value = "/{id}")
-@ResponseStatus(value = HttpStatus.NO_CONTENT)
-public void delete(@PathVariable int id) {
-    super.delete(id);
-}
+    @Override
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
 
-@PostMapping
-@ResponseStatus(value = HttpStatus.NO_CONTENT)
-public void createOrUpdate(@RequestParam Integer id,
-                           @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
-                           @RequestParam String description,
-                           @RequestParam int calories) {
-    Meal meal = new Meal(id, dateTime, description, calories);
-    if (meal.isNew()) {
-    super.create(meal);
-}
-}
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createOrUpdate(@RequestParam Integer id,
+                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
+                               @RequestParam String description,
+                               @RequestParam int calories) {
+        Meal meal = new Meal(id, dateTime, description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
 
-@Override
-@GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
-public List<MealTo> getBetween(
-                                @RequestParam(required = false) LocalDate startDate,
-                                @RequestParam(required = false) LocalTime startTime,
-                                @RequestParam(required = false) LocalDate endDate,
-                                @RequestParam(required = false) LocalTime endTime) {
-    return super.getBetween(startDate, startTime, endDate, endTime);
-}
+    @Override
+    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getBetween(
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalTime startTime,
+            @RequestParam(required = false) LocalDate endDate,
+            @RequestParam(required = false) LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
 }
